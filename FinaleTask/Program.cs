616a01e735349b7a9848edd03751c484f0efe08e
Ana <anastasia50192iv@gmail.com>
2022-10-28 19:58:39 +0300

    Added second step

--- FinaleTask/Program.cs
+++ FinaleTask/Program.cs
@@ -31,3 +31,24 @@ switch (fromUser)
         break;
 }
 
+int lenNewArray = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3) lenNewArray++;
+}
+
+string[] newArray = new string[lenNewArray];
+int idx = 0;
+
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[idx] = array[i];
+        idx++;
+    }
+}
+
+PrintArray(array);
+Console.Write("→ ");
+PrintArray(newArray);
